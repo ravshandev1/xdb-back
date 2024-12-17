e5918cb7f5ce094406d8ecce00f89ebbfd65e187
Ravshan Giyosov <97965795+ravshandev1@users.noreply.github.com>
2024-12-18 00:24:27 +0500

    Update tasks.py

--- back/tasks.py
+++ back/tasks.py
@@ -13,28 +13,30 @@ app.autodiscover_tasks()
 
 
 @shared_task
-def send_data_to_tax_task(ls: list[dict], ids: list[dict]):
+def send_data_to_tax_task(ls: list[dict]):
     token = post(f"{ENV.get('TAX_API')}/water-supply/api/authenticate/login", json={"username": "WaterSupply", "password": "Pa$$w0rd"})
-    for i, j in zip(ls, ids):
+    for i in ls:
         res = post(f"{ENV['TAX_API']}/xdduk-api/xdduk-api/involved-businessman", json=i,
                    headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token.text}'})
-        print(res.text)
         if res.status_code >= 500:
             status = "Soliqni API si ishlamadi!"
         elif res.status_code >= 400:
             status = res.json()["text"]
         else:
             status = "Muvofiqiyatli"
+    return "Got the response"
+@shared_task
+def get_data_from_tax_task(ls: list[dict], ids: list[dict]):
+    token = post(f"{ENV.get('TAX_API')}/water-supply/api/authenticate/login", json={"username": "WaterSupply", "password": "Pa$$w0rd"})
+    for i, j in zip(ls, ids):
         res = get(f"{ENV.get('TAX_API')}/water-supply/api/water-supply/get-gravel-info",
                   headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token.text}'},
                   params={"tin": i['tin'], "periodYear": j['year'], "periodMonth": j['month']})
-        print(res.text)
         if res.status_code >= 500:
             diff_count = "Soliqni API si ishlamadi!"
         elif res.status_code >= 400:
             diff_count = res.json()["text"]
         else:
-            diff_count = res.json()['data']['count'] if res.json()['data']['count'] else "Malumot olishda xatolik"
+            diff_count = res.json()['data']['count'] if res.json()['data'] else "Malumot olishda xatolik"
         res = patch(f"{ENV.get('BASE_URL')}/application/{j['id']}", data={"status": status, "diff_count": diff_count})
-        print(res.text)
     return "Got the response"
